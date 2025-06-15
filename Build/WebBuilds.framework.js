902d80cd616cc7399a2c212ba305b874c7a59a7f
Ufuk Bal <balufuk91@gmail.com>
2025-06-15 17:36:17 +0200

    WebGL build 15.06

--- Build/WebBuilds.framework.js
+++ Build/WebBuilds.framework.js
@@ -1283,10 +1283,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7842224: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7842279: ($0) => { performance.now = function() { return $0; }; },  
- 7842327: ($0) => { performance.now = function() { return $0; }; },  
- 7842375: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7842256: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7842311: ($0) => { performance.now = function() { return $0; }; },  
+ 7842359: ($0) => { performance.now = function() { return $0; }; },  
+ 7842407: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- Build/WebBuilds.loader.js
+++ Build/WebBuilds.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 97148236,
+    wasmFileSize: 97148379,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
