417e8e1f4da315e18d8f7d2719d872b2de65eb73
Ufuk Bal <balufuk91@gmail.com>
2025-06-19 13:34:38 +0200

    WebGL build 19.06

--- Build/WebBuilds.loader.js
+++ Build/WebBuilds.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 97302633,
+    wasmFileSize: 97330271,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
